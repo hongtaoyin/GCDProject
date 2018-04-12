42b696e9613fa07bc45230cf50c4c613f0aa583e
Yinht <244509448@qq.com>
2018-04-12 10:36:20 +0800

    Update

---
+++ GCDProject/GCDProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDProject
+//
+//  Created by Yinht on 2018/4/12.
+//  Copyright © 2018年 yinht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDProject/GCDProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDProject
+//
+//  Created by Yinht on 2018/4/12.
+//  Copyright © 2018年 yinht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
